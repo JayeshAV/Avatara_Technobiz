b7896c2509ec9bc54f0bd17e07fed78bc5bf8920
JayeshAV <bja.avataratechnobiz@gmail.com>
2025-01-28 12:16:56 +0530

    lastdone

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,84 +1,94 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    return (
-        <>
-            <div className='w-full bg-[#091E3E]'>
-                <main className='container-fluid w-[85%] h-92 mx-auto mt-10'>
-                    <div className="flex flex-wrap justify-between text-white">
+  return (
+    <div className="w-full bg-[#091E3E]">
+      <main className="container-fluid w-[85%] mx-auto mt-10 py-10">
+        <div className="flex flex-wrap justify-between text-white">
+          {/* Column 1: About Section */}
+          <div className="w-full md:w-1/4 text-center bg-[#06A3DA] py-5">
+            <div className="py-5 m-auto icon">
+              <img src="src/assets/favicon.ico" className="mx-auto" alt="Logo" />
+            </div>
+            <div className="p-5 para">
+              We specialize in providing innovative, results-driven services that are tailored to meet the unique needs of our global clientele. With a strong focus on delivering projects on time, we help businesses achieve their digital goals by combining cutting-edge technology with creative excellence.
+            </div>
+          </div>
 
-                        <div className="w-full md:w-1/4 text-center bg-[#06A3DA] h-full py-5">
-                            <div className="icon m-auto py-5">
-                                <img src="src/assets/favicon.ico" className='mx-auto' alt="Logo" />
-                            </div>
-                            <div className="para p-5">
-                                We specialize in providing innovative, results-driven services that are tailored to meet the unique needs of our global clientele. With a strong focus on delivering projects on time, we help businesses achieve their digital goals by combining cutting-edge technology with creative excellence.
-                            </div>
-                        </div>
+          {/* Column 2: Contact Information */}
+          <div className="w-full py-5 text-left md:w-1/4 lg:ps-5">
+            <ul>
+              <li className="p-2 py-8 text-3xl font-medium">Get In Touch</li>
+              <a href="https://www.google.com/maps/place/Avatara+Technobiz+LLP">
+                <li className="flex items-center py-1">
+                  <i className="p-2 text-xl text-sky-200 fa-solid fa-location-dot"></i>
+                  Office No. 14, 5th floor, Madhuram Arcade-2, Near Madhuram Circle Dindoli, Surat, 394210
+                </li>
+              </a>
+              <a href="mailto:avataratechnobiz@gmail.com">
+                <li className="flex items-center py-1">
+                  <i className="p-2 text-xl text-sky-200 fa-solid fa-envelope"></i>
+                  avataratechnobiz@gmail.com
+                </li>
+              </a>
+              <a href="tel:+919558476369">
+                <li className="flex items-center">
+                  <i className="p-2 text-xl text-sky-200 fa-solid fa-phone"></i>
+                  +91 9558476369
+                </li>
+              </a>
+            </ul>
+            <div className="m-2 contact-icons">
+              <ul className="flex justify-between w-1/3">
+                <a href="mailto:avataratechnobiz@gmail.com">
+                  <li><i className="p-2 bg-blue-500 rounded-lg fa-solid fa-envelope"></i></li>
+                </a>
+                <a href="https://www.linkedin.com/company/avatara-technobiz-llp/posts/?feedView=all">
+                  <li><i className="p-2 bg-blue-500 rounded-lg fa-brands fa-linkedin"></i></li>
+                </a>
+                <a href="https://www.instagram.com/avataratechnobiz/">
+                  <li><i className="p-2 bg-blue-500 rounded-lg fa-brands fa-instagram"></i></li>
+                </a>
+              </ul>
+            </div>
+          </div>
 
-                        <div className="w-full md:w-1/4 text-left h-full py-5 lg:ps-5">
-                            <ul>
-                                <li className='p-2 text-3xl py-8 font-medium'>Get In Touch</li>
-                                <a href="https://www.google.com/maps/place/Avatara+Technobiz+LLP/@21.140643,72.8845383,17z/data=!4m15!1m8!3m7!1s0x3be0515a0f81fa81:0x4847daa352709464!2sAvatara+Technobiz+LLP!8m2!3d21.1407758!4d72.8843018!10e5!16s%2Fg%2F11wtdy2fs9!3m5!1s0x3be0515a0f81fa81:0x4847daa352709464!8m2!3d21.1407758!4d72.8843018!16s%2Fg%2F11wtdy2fs9?authuser=0&entry=ttu&g_ep=EgoyMDI1MDEyMi4wIKXMDSoASAFQAw%3D%3D">
-                                    <li className='flex py-1 items-center me-5'><i className="text-sky-200 text-xl p-2 fa-solid fa-location-dot"></i> Office No. 14, 5th floor, Madhuram Arcade-2, Near Madhuram Circle Dindoli, Surat, 394210</li>
-                                </a>
-                                <a href="mailto:avataratechnobiz@gmail.com">
-                                    <li className='flex py-1 items-center'><i className="text-sky-200 text-xl p-2 fa-solid fa-envelope"></i>avataratechnobiz@gmail.com</li>
-                                </a>
-                                <a href="tel:+919558476369">
-                                    <li className='flex items-center'><i className="text-sky-200 text-xl p-2 fa-solid fa-phone"></i> +91 9558476369</li>
-                                </a>
-                            </ul>
-                            <div className="contact-icons m-2">
-                                <ul className='flex justify-between w-1/3'>
-                                    <a href="mailto:avataratechnobiz@gmail.com">
-                                        <li><i className="bg-blue-500 rounded-lg p-2 fa-solid fa-envelope"></i></li>
-                                    </a>
-                                    <a href="https://www.linkedin.com/company/avatara-technobiz-llp/posts/?feedView=all">
-                                        <li><i className="bg-blue-500 rounded-lg p-2 fa-brands fa-linkedin"></i></li>
-                                    </a>
-                                    <a href="https://www.instagram.com/avataratechnobiz/">
-                                        <li><i className="bg-blue-500 rounded-lg p-2 fa-brands fa-instagram"></i></li>
-                                    </a>
-                                </ul>
-                            </div>
-                        </div>
+          {/* Column 3: Quick Links */}
+          <div className="w-full py-5 text-left md:w-1/4 lg:ps-5">
+            <ul>
+              <li className="py-2 py-8 text-3xl font-medium">Quick Links</li>
+              <Link to="/"><li className="py-1"><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right"></i>Home</li></Link>
+              <Link to="/about"><li className="py-2"><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right"></i>About</li></Link>
+              <Link to="/service"><li className="py-2"><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right"></i>Service</li></Link>
+              <Link to="/career"><li className="py-2"><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right"></i>Career</li></Link>
+              <Link to="/contact"><li className="py-2"><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right"></i>Contact Us</li></Link>
+            </ul>
+          </div>
 
-                        {/* Column 3 */}
-                        <div className="w-full md:w-1/4 text-left h-full py-5 lg:ps-5">
-                            <ul>
-                                <li className='py-2 text-3xl py-8 font-medium'>Quick Links</li>
-                                <Link to={"/"}> <li className='py-1'><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right animate__animated animate__backInLeft"></i>Home</li></Link>
-                                <Link to={"/about"}><li className='py-2'><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right animate__animated animate__backInLeft"></i>About</li></Link>
-                                <Link to={"/service"}><li className='py-2'><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right animate__animated animate__backInLeft"></i>Service</li></Link>
-                                <Link to={"/career"}><li className='py-2'><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right animate__animated animate__backInLeft"></i>Career</li></Link>
-                                <Link to={"/contact"}><li className='py-2'><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right animate__animated animate__backInLeft"></i>Contact Us</li></Link>
-                            </ul>
-                        </div>
+          {/* Column 4: Popular Links */}
+          <div className="w-full py-5 text-left md:w-1/4 lg:pe-5">
+            <ul>
+              <li className="py-2 py-8 text-3xl font-medium">Popular Links</li>
+              <Link to="/about"><li className="py-2"><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right"></i>Gallery</li></Link>
+              <Link to="/service"><li className="py-2"><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right"></i>Features</li></Link>
+              <Link to="/contact"><li className="py-2"><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right"></i>Privacy Policy</li></Link>
+              <Link to="/service"><li className="py-2"><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right"></i>Services</li></Link>
+            </ul>
+          </div>
+        </div>
+      </main>
 
-                        {/* Column 4 */}
-                        <div className="w-full md:w-1/4 text-left h-full py-5 lg:pe-5">
-                            <ul>
-                                <li className='py-2 text-3xl py-8 font-medium'>Popular Links</li>
-                                <Link to={"/about"}><li className='py-2'><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right animate__animated animate__backInLeft"></i>Gallery</li></Link>
-                                <Link to={"/service"}><li className='py-2'><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right animate__animated animate__backInLeft"></i>Features</li></Link>
-                                <Link to={"/contact"}><li className='py-2'><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right animate__animated animate__backInLeft"></i>Privacy Policy</li></Link>
-                                <Link to={"/service"}><li className='py-2'><i className="px-2 text-sm text-sky-200 fa-solid fa-arrow-right animate__animated animate__backInLeft"></i>Services</li></Link>
-                            </ul>
-                        </div>
-                    </div>
+      {/* Last Contact Bar */}
+      <div className="w-full bg-slate-900">
+        <div className="h-20 py-7">
+          <h2 className="text-center text-white">
+            <a href="/">© avataratechnobiz.com.</a> All Rights Reserved. Designed by <a href="/">Avatara TechnoBiz</a>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-                </main>
-                  {/* last contact bar */}
-                    
-
-                <div className='w-full'>
-                    <div className='h-20 bg-slate-900 py-7'>
-                      <h2 className='text-center text-white'><a href="/">© avataratechnobiz.com.</a> All Rights Reserved. Designed by <a href="/"> Avatara TechnoBiz </a></h2>
-                    </div></div>
-            </div></>
-    )
-}
-
-export default Footer
+export default Footer;
--- src/components/Home_Pages/Reviews.jsx
+++ src/components/Home_Pages/Reviews.jsx
@@ -25,11 +25,8 @@ const Reviews = () => {
       "name": "Alice Smith",
       "title": "Co-Founder",
       "image": "https://usercontent.one/wp/manforhimself.com/wp-content/uploads/2022/09/MFH-Featured-Square-1200-x-1200-1.jpeg?media=1728223447"
-    },
-   
-    
-  ]
-  ;
+    }
+  ];
 
   const settings = {
     autoplay: true,
@@ -37,22 +34,22 @@ const Reviews = () => {
     speed: 800,
     draggable: true,
     infinite: true,
-    slidesToShow: 3, // Default to showing 3 slides
+    slidesToShow: 3,
     slidesToScroll: 1,
     arrows: false,
     dots: false,
     responsive: [
       {
-        breakpoint: 1024, // For screens less than 1024px (e.g., tablets)
+        breakpoint: 1024, 
         settings: {
-          slidesToShow: 2, // Show 2 slides
+          slidesToShow: 2, 
           slidesToScroll: 1
         }
       },
       {
-        breakpoint: 575, // For screens less than 575px (e.g., mobile)
+        breakpoint: 575, 
         settings: {
-          slidesToShow: 1, // Show 1 slide
+          slidesToShow: 1, 
           slidesToScroll: 1
         }
       }
@@ -61,26 +58,29 @@ const Reviews = () => {
 
   return (
     <main className="container-fluid w-[85%] p-2 mx-auto">
-      <section className="flex justify-center items-center min-h-screen">
+      <section className="flex items-center justify-center min-h-screen">
         <div className="container px-4 py-8">
-          <div className="text-center pb-5">
-            <h2 className="text-blue-500 font-semibold text-3xl mt-5">Testimonial</h2>
-            <h1 className="text-4xl font-bold text-gray-800 mt-3 mb-5">
+          <div className="pb-5 text-center">
+            <h2 className="mt-5 text-3xl font-semibold text-blue-500">Testimonial</h2>
+            <h1 className="mt-3 mb-5 text-4xl font-bold text-gray-800">
               What Our Clients Say About Our Digital Services
             </h1>
           </div>
-          <Slider {...settings} className="testimonial-slider mt-5">
+          <Slider {...settings} className="mt-5 testimonial-slider">
             {testimonials.map((testimonial) => (
-              <div key={testimonial.id} className="testimonial-slide animate__animated animate__pulse animate__slower animate__delay-0.4s animate__infinite" >
-                <div className="p-1 border-2 border-[#3B82F6]-200 rounded-xl  text-center w-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-2/3 mx-auto">
+              <div 
+                key={testimonial.id} 
+                className="testimonial-slide animate__animated animate__pulse animate__slower animate__delay-0.4s animate__infinite"
+              >
+                <div className="p-4 border-1 border-[#3B82F6]-200 rounded-xl text-center w-full h-[400px] flex flex-col justify-between">
                   <div className="py-2">
                     <i className="fas fa-quote-right text-[#14213d] text-2xl"></i>
                   </div>
-                  <div className="w-1/3 mx-auto animate__animated animate__fadeIn animate__delay-0.4s ">
+                  <div className="w-24 h-24 mx-auto mb-4 animate__animated animate__fadeIn">
                     <img
                       src={testimonial.image}
                       alt={testimonial.name}
-                      className="w-20 h-20 md:w-24 md:h-24 rounded-full border-2 border-[#e5e5e5]"
+                      className="w-full h-full object-cover rounded-full border-2 border-[#e5e5e5]"
                     />
                   </div>
                   <div className="py-5">
@@ -97,11 +97,7 @@ const Reviews = () => {
                 </div>
               </div>
             ))}
-          
-        
           </Slider>
-
-
         </div>
       </section>
     </main>
