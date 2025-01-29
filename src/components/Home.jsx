1a24722364ee8e5d431858faa60f8d0c0e33e255
JayeshAV <bja.avataratechnobiz@gmail.com>
2025-01-29 14:16:39 +0530

    done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
       <div id="header-carousel" className="mb-5 carousel slide carousel-fade" data-bs-ride="carousel">
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img className="w-100" src="https://www.michaelpage.co.in/sites/michaelpage.com.sg/files/inline-images/How%20to%20write%20employee%20testimonials%202_0.jpg" alt="Image" />
+            <img className="w-100" src="src/assets/carousel-1.jpg" alt="Image" />
             <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
               <div className="p-3" style={{ maxWidth: '900px' }}>
                 <h5 className="mb-3 text-white text-uppercase animated slideInDown">
@@ -34,7 +34,7 @@ const Home = () => {
             </div>
           </div>
           <div className="carousel-item">
-            <img className="w-100" src="https://media.licdn.com/dms/image/v2/C5612AQFMCF78-Wfjkw/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1572341413864?e=2147483647&v=beta&t=IVNcYZMp7aYmnlqmE0w24YbGl1sVfrNhs8O41rIMl_k" alt="Image" />
+            <img className="w-100" src="src/assets/carousel-2.jpg" alt="Image" />
             <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
               <div className="p-3" style={{ maxWidth: '900px' }}>
                 <h5 className="mb-3 text-white text-uppercase animated slideInDown">
